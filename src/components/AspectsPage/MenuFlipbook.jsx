9343088be72eaa8e58a62a8869d08e648ef6fc70
shandiru <msddiru@gmail.com>
2025-09-17 18:49:34 +0530

    addeed new menu

--- src/components/AspectsPage/MenuFlipbook.jsx
+++ src/components/AspectsPage/MenuFlipbook.jsx
@@ -184,7 +184,7 @@ export default function MenuFlipbook() {
     arr.push(
       <CoverPage
         key="cover"
-        restaurant="Al Naseeb"
+        restaurant="ASPECTS"
         tagline="Cuban-American Tapas, Cocktails & Good Times"
       />
     );
